0237e17e3747ac614519ecaec9b4a758c3ab606d
marjanhasan <moonmarjanhasan@gmail.com>
2023-04-10 01:33:45 +0600

    banner section done with mobile responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="bg-gray-100 ">
-      <div className="flex items-center justify-between px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="flex items-center justify-between relative px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         {/* logo section  */}
         <Link to="/" className="">
           <span className="ml-2 text-2xl font-bold tracking-wide txt-color">
@@ -64,7 +64,7 @@ const Header = () => {
           </button>
           {isMenuOpen && (
             <div className="absolute top-0 left-0 w-full z-10">
-              <div className="p-5 bg-white border rounded shadow-sm">
+              <div className="p-5 bg-red-50 border rounded shadow-sm">
                 {/* logo & button section  */}
                 <div className="flex items-center justify-between mb-4">
                   <div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <div className="bg-gray-100">
+        <div className="my-container flex flex-col items-center justify-between lg:flex-row h-full">
+          {/* left side  */}
+          <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
+            <div className="max-w-xl mb-6 lg:mt-8">
+              <h2 className="max-w-lg mb-6 font-sans lg:text-6xl font-bold tracking-tight text-gray-900 text-4xl sm:text-5xl sm:leading-none">
+                One Step <br className="hidden md:block" /> Closer To Your
+                <br />
+                <span className="inline-block txt-color">Dream Job</span>
+              </h2>
+              <p className="text-base text-gray-500 md:text-lg">
+                Books are a uniquely portable magic. Books serve to show a man
+                that those original thoughts of his aren't very new after all.
+                The man who does not read good books is no better than the man
+                who can't.
+              </p>
+              <Link to="/" className="btn md:w-auto md:mr-4 mt-6">
+                <div className="inline-flex items-center justify-center w-full h-full">
+                  <p className="mr-3">Visit Store</p>
+                </div>
+              </Link>
+            </div>
+          </div>
+          {/* right side  */}
+          <div className="lg:w-1/2">
+            <div className="w-full lg:w-4/5 lg:ml-auto m-auto lg:h-[80%]">
+              <img
+                src="pngwing.com.png"
+                alt=""
+                className="object-fill h-[80%] w-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
